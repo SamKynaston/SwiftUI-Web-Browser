3873ee54adaee4270324db1ffe2b99a0c64e938f
samkynaston <samkynno@icloud.com>
2026-09-10 20:25:08 +0100

    Useragent changes only when on macOS (temporary fix, plan to implement a settings page where custom useragents can be defined)

--- Project/Models/TabModel.swift
+++ Project/Models/TabModel.swift
@@ -17,11 +17,13 @@ private func makeWebView() -> WKWebView {
         configuration: configuration
     )
 
+    #if os(macOS)
     webView.customUserAgent =
         "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) " +
         "AppleWebKit/605.1.15 (KHTML, like Gecko) " +
         "Version/27.0 Safari/605.1.15"
-
+    #endif
+    
     return webView
 }
 
